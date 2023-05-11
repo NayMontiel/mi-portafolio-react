6910f6f45052f87a0b7963c7c1fd5bac0ee428f2
NayMontiel <nay.montiel0721@gmail.com>
2023-05-11 19:50:35 -0400

    actualizando

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,6 @@ export * from './NavBar';
 export * from './Footer';
 export * from './Carrusel';
 export * from './MiInfo';
-export * from './ProgressBar';
 export * from './InfoContacto';
 export * from './CardsPortafolio';
 export * from './Banner';
--- src/router/AcercaDe.jsx
+++ src/router/AcercaDe.jsx
@@ -1,20 +1,18 @@
 import foto from '../assets/img/foto.png'
-import { MiInfo, ProgressBar } from '../components'
+import { MiInfo,  } from '../components'
 
 export const AcercaDe = () => {
   return (
     <div className='container'>
          <div className="row acerca-de align-items-center px-4 mb-5 animate__animated animate__fadeInUp">
-                <div className="col-12 col-lg-4 foto text-center" >
-                      <img id="foto" src={foto} className="rounded-circle mb-3" alt="" />
+                <div className="col-12 col-lg-4 foto text-center " >
+                      <img id='foto' src={foto} className="rounded-circle mb-3 " alt="" />
                       <p className="name">Nairyn Montiel</p>
                 </div>
 
-                <div className="col-12 col-lg-8 info">
+                <div className="col-12 col-lg-8 info ">
                  
                  <MiInfo />
-
-                 <ProgressBar />
 
                 </div>
           </div>
